ebc34b3577f661289eb7d1b12df59b6138ce21f5
Thanapong Yingyong <thanapongtle@gmail.com>
2019-03-21 15:40:55 +0700

    layout button: frame layout

--- app/src/main/java/com/thanapong/android_ohek/MainActivity.kt
+++ app/src/main/java/com/thanapong/android_ohek/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.thanapong.android_ohek
 
+import android.app.Application
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (i in 1..9) {
+            var bt = arrayOf(i)
+            var name_bt = "bt+$i"
+            print(name_bt)
+            Toast.makeText(this, name_bt, Toast.LENGTH_LONG).show()
+        }
     }
 }
