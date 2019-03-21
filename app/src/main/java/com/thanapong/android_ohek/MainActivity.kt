edbb9cf1f4744a639a077cccd9f878b0c709c728
Thanapong Yingyong <thanapongtle@gmail.com>
2019-03-21 19:13:20 +0700

    update model: add check winner

--- app/src/main/java/com/thanapong/android_ohek/MainActivity.kt
+++ app/src/main/java/com/thanapong/android_ohek/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.thanapong.android_ohek
 
 import android.annotation.SuppressLint
-import android.app.Application
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import com.thanapong.android_ohek.Model.Board
 import com.thanapong.android_ohek.Model.CountClick
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +15,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var win = false
+
+        ButtonBaord()
+
+        Toast.makeText(this, Board.bt_1_check.toString(), Toast.LENGTH_LONG).show()
+
+//        if (Board.bt_1_check == Board.bt_2_check && Board.bt_2_check == Board.bt_3_check){
+//            Toast.makeText(this, "win", Toast.LENGTH_LONG).show()
+//        }
+
+    }
+
+    fun ButtonBaord() {
         val str_o = "O"
         val str_x = "X"
 
@@ -25,9 +36,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_1.text = str_o
+                    Board.bt_1_check = 0
                 }
                 else {
                     bt_1.text = str_x
+                    Board.bt_1_check = 1
                 }
             }
         }
@@ -37,9 +50,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_2.text = str_o
+                    Board.bt_2_check = 0
                 }
                 else {
                     bt_2.text = str_x
+                    Board.bt_2_check = 1
                 }
             }
         }
@@ -49,9 +64,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_3.text = str_o
+                    Board.bt_3_check = 0
                 }
                 else {
                     bt_3.text = str_x
+                    Board.bt_3_check = 1
                 }
             }
         }
@@ -61,9 +78,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_4.text = str_o
+                    Board.bt_4_check = 0
                 }
                 else {
                     bt_4.text = str_x
+                    Board.bt_4_check = 1
                 }
             }
         }
@@ -73,9 +92,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_5.text = str_o
+                    Board.bt_5_check = 0
                 }
                 else {
                     bt_5.text = str_x
+                    Board.bt_5_check = 1
                 }
             }
         }
@@ -85,9 +106,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_6.text = str_o
+                    Board.bt_6_check = 0
                 }
                 else {
                     bt_6.text = str_x
+                    Board.bt_6_check = 1
                 }
             }
         }
@@ -97,9 +120,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_7.text = str_o
+                    Board.bt_7_check = 0
                 }
                 else {
                     bt_7.text = str_x
+                    Board.bt_7_check = 1
                 }
             }
         }
@@ -109,9 +134,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_8.text = str_o
+                    Board.bt_8_check = 0
                 }
                 else {
                     bt_8.text = str_x
+                    Board.bt_8_check = 1
                 }
             }
         }
@@ -121,9 +148,11 @@ class MainActivity : AppCompatActivity() {
                 CountClick.count()
                 if (CountClick.countClick%2 == 0) {
                     bt_9.text = str_o
+                    Board.bt_9_check = 0
                 }
                 else {
                     bt_9.text = str_x
+                    Board.bt_9_check = 1
                 }
             }
         }
--- app/src/main/java/com/thanapong/android_ohek/Model/Board.kt
+++ app/src/main/java/com/thanapong/android_ohek/Model/Board.kt
@@ -1,4 +1,18 @@
 package com.thanapong.android_ohek.Model
 
+import android.widget.Toast
+
 class Board {
+
+    companion object Committee {
+        var bt_1_check:Byte = 0
+        var bt_2_check:Byte = 0
+        var bt_3_check:Byte = 0
+        var bt_4_check:Byte = 0
+        var bt_5_check:Byte = 0
+        var bt_6_check:Byte = 0
+        var bt_7_check:Byte = 0
+        var bt_8_check:Byte = 0
+        var bt_9_check:Byte = 0
+    }
 }
